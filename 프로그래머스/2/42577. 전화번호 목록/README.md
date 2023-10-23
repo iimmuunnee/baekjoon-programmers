cfbc7c5193794f17dc985eecb561cdd32b30ceaa
Keep going <51280476+iimmuunnee@users.noreply.github.com>
2023-10-23 12:01:14 +0900

    [level 2] Title: 전화번호 목록, Time: 95.69 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/README.md
+++ 프로그래머스/2/42577. 전화번호 목록/README.md
@@ -1,10 +1,10 @@
 # [level 2] 전화번호 목록 - 42577 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577) 
 
 ### 성능 요약
 
-메모리: 76.2 MB, 시간: 115.62 ms
+메모리: 76.5 MB, 시간: 95.69 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 10월 1일 11:30:40
+2023년 10월 1일 12:1:11
 
 ### 문제 설명
 
--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -1,13 +1,13 @@
 function solution(phone_book) {
-    var answer = true;
+    var result = true;
     phone_book.sort() // 정렬
     for (let i = 0; i < phone_book.length - 1; i++){
         if(phone_book[i].length < phone_book[i+1].length){
             if(phone_book[i] == phone_book[i+1].slice(0, phone_book[i].length)){
-                answer = false
+                result = false
                 break
             }
         }
     }
-    return answer;
+    return result;
 }
