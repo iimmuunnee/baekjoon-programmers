eef5bad84ae9f967b9420a186037fea7d55f81bc
Keep going <51280476+iimmuunnee@users.noreply.github.com>
2023-10-23 11:30:43 +0900

    [level 2] Title: 전화번호 목록, Time: 115.62 ms, Memory: 76.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -0,0 +1,13 @@
+function solution(phone_book) {
+    var answer = true;
+    phone_book.sort() // 정렬
+    for (let i = 0; i < phone_book.length - 1; i++){
+        if(phone_book[i].length < phone_book[i+1].length){
+            if(phone_book[i] == phone_book[i+1].slice(0, phone_book[i].length)){
+                answer = false
+                break
+            }
+        }
+    }
+    return answer;
+}
