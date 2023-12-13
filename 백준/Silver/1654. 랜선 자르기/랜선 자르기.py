02c1f9e30fb55ecf38aed702b7745da41429704e
Keep going <51280476+iimmuunnee@users.noreply.github.com>
2023-12-13 21:00:55 +0900

    [Silver II] Title: 랜선 자르기, Time: 468 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
@@ -0,0 +1,22 @@
+from sys import stdin
+
+k, n = map(int, stdin.readline().split())
+arr = []
+
+for i in range(k):
+  arr.append(int(input()))
+
+start = 1
+end = max(arr)
+
+while (start <= end):
+    mid = (start + end) // 2
+    cnt = 0
+    for i in range(k):
+        cnt += arr[i] // mid
+    if cnt >= n:
+        start = mid + 1
+    else:
+        end = mid - 1
+      
+print(end)
