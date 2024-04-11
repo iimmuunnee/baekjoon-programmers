4910d03264b30ec1a771b28a41b11990358e9996
Keep going <51280476+iimmuunnee@users.noreply.github.com>
2024-04-11 14:07:08 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.03 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
@@ -0,0 +1,16 @@
+def solution(bridge_length, weight, truck_weights):
+    time = 0
+    p = list(truck_weights) # 트럭 무게
+    x = [0] * bridge_length # 다리
+    s = 0
+    while (x):
+        time += 1
+        s -= x.pop(0) # s: 다리 위의 총 트럭 무게
+        if p:
+            if ( s + p[0] ) <= weight:
+                s += p[0] # 여유되면 트럭 추가
+                x.append(p.pop(0)) # 다리에 트럭 추가  
+            else:
+                x.append(0) # 여유 없으면 무게 0 추가
+
+    return time
