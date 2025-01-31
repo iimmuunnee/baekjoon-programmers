ca70f04adca75a8a6113ea7b69b0833f6d9c7227
Keep going <51280476+iimmuunnee@users.noreply.github.com>
2025-01-31 17:23:12 +0900

    [Bronze I] Title: 약수, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/1037. 약수/약수.py
+++ 백준/Bronze/1037. 약수/약수.py
@@ -2,6 +2,12 @@ n = int(input())
 n_list = list(map(int, input().split(" ")))
 
 if n == 1:
-    print(n_list[0]**2)
+    print(n_list[0] ** 2)
 else:
-    print(max(n_list) * min(n_list))
+    max_val, min_val = n_list[0], n_list[0]  # 첫 번째 요소를 기준으로 최댓값과 최솟값을 설정
+    for num in n_list[1:]:
+        if num > max_val:
+            max_val = num
+        if num < min_val:
+            min_val = num
+    print(max_val * min_val)
