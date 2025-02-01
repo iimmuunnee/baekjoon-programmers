2b075861569bc6788277ec148a47a7367c9f8a29
Keep going <51280476+iimmuunnee@users.noreply.github.com>
2025-02-01 15:22:40 +0900

    [Bronze V] Title: 새싹, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.py
@@ -0,0 +1,8 @@
+import sys
+
+sys.stdout.write("         ,r'\"7\n"
+                 "r`-_   ,'  ,/\n"
+                 " \. \". L_r'\n"
+                 "   `~\\/\n"
+                 "      |\n"
+                 "      |\n")
