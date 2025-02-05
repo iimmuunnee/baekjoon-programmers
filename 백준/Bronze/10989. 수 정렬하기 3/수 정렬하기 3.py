0a4ea5d2a78fabf56912dfbb1d25529a39cbfcff
Keep going <51280476+iimmuunnee@users.noreply.github.com>
2025-02-05 15:53:50 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 8676 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
@@ -1,15 +1,18 @@
-import sys 
-N = int(sys.stdin.readline().rstrip())
+import sys
+
+n = int(sys.stdin.readline().rstrip())
 cnt_dic = {}
-for _ in range(N):
-    X =int(sys.stdin.readline().rstrip())   
-    if X in cnt_dic: 
-        cnt_dic[X] += 1
-    else:
-        cnt_dic[X] = 1
-for i in range(max(cnt_dic.keys())+1):
-    if i not in cnt_dic:
-        continue
-    else:
-        for _ in range(cnt_dic[i]):
-            print(i)
+
+for i in range(n):
+  num = int(sys.stdin.readline().rstrip())
+  if num in cnt_dic:
+    cnt_dic[num] += 1
+  else:
+    cnt_dic[num] = 1 
+
+for i in range(max(cnt_dic.keys()) + 1):
+  if i in cnt_dic:
+    for _ in range(cnt_dic[i]):
+      sys.stdout.write((str(i) + "\n"))
+  else:
+    continue
