3721ac3b3d0f5a936f9771debc9630055a5d37a2
Keep going <51280476+iimmuunnee@users.noreply.github.com>
2025-02-09 20:53:59 +0900

    [Silver IV] Title: 카드2, Time: 176 ms, Memory: 51848 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.py
@@ -0,0 +1,14 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+
+n = int(input())
+
+queue = deque(i for i in range(1, n + 1))
+
+while len(queue) > 1:
+    queue.popleft()
+    queue.append(queue.popleft())
+
+sys.stdout.write(str(queue[0]))
