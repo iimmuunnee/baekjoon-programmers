5c3f832332644b328b2d13fa2744e76a32bbead3
Keep going <51280476+iimmuunnee@users.noreply.github.com>
2025-02-09 21:50:09 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 64 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.py
@@ -0,0 +1,34 @@
+import sys
+
+input = sys.stdin.readline
+
+while True:
+    sentence = input().rstrip()
+
+    if sentence == ".":
+        break
+
+    stack = []
+    is_balanced = True
+
+    for char in sentence:
+        if char in "([":
+            stack.append(char)
+        elif char == ")":
+            if stack and stack[-1] == "(": # stack이 비어있지않고 가장 뒷 괄호가 같은 종류의 여는 괄호인지
+                stack.pop() # 완벽하게 된 괄호는 삭제
+            else:
+                is_balanced = False
+                break
+        
+        elif char == "]":
+            if stack and stack[-1] == "[":
+                stack.pop()
+            else:
+                is_balanced = False
+                break
+        
+    if is_balanced and not stack:
+        sys.stdout.write("yes\n")
+    else:
+        sys.stdout.write("no\n")
