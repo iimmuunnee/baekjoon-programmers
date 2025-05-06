71d4895bfb2d33f702676b1d35e874924cc6c66f
Keep going <51280476+iimmuunnee@users.noreply.github.com>
2025-05-06 21:17:16 +0900

    [Silver I] Title: IOIOI, Time: 268 ms, Memory: 33388 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.py
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -1,20 +1,26 @@
 import sys
-import re
 
 n = int(sys.stdin.readline()) # n + 1개의 I, n개의 O
 m = int(sys.stdin.readline()) # S의 길이
 s = sys.stdin.readline() # 주어진 문자열
 
-Pn = "IO" * n + "I" # n번째 문자열 형태
+i = 0
 cnt = 0
+pn_cnt = 0
 
-# 직접 구현
-for i in range(m - len(Pn) + 1):
-    if s[i:i+len(Pn)] == Pn:
-        cnt += 1
+while i < m - 1:
+    if s[i] == "I" and s[i+1] == "O":
+        pn_cnt = 0
 
-# re 라이브러리 사용
-matches = re.findall(f"(?={Pn})", s)
-cnt = len(matches)
+        while i + 2 < m and s[i+1] == "O" and s[i+2] == "I":
+            pn_cnt += 1
+            i += 2
+
+            if pn_cnt >= n:
+                cnt += 1
+
+        i += 1
+    else:
+        i += 1
 
 sys.stdout.write(str(cnt))
