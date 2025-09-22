b77a3a112aefce0d247512457157c4794cebb91c
Keep going <51280476+iimmuunnee@users.noreply.github.com>
2025-09-22 20:23:32 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 붕대 감기, Time: 0.48 ms, Memory: 9.29 MB -BaekjoonHub

--- 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.py
+++ 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.py
@@ -1,29 +1,28 @@
 def solution(bandage, health, attacks):
     answer = 0
-    last_time = attacks[-1][0] # 마지막 시간
-    continuous_success = 0 # 연속 성공
+    last_time = attacks[-1][0] # 마지막 공격시간
     hp = health # 현재 체력
-    casting_time = bandage[0] # 총 시전 시간
-    heal = bandage[1] # 초당 힐량
-    add_heal = bandage[2] # 시전 완료시 추가 힐
-    atk_idx = 0 # 다음은 몇번째 공격인지
+    continuous_success = 0 # 연속 회복
+    casting_time = bandage[0]
+    heal = bandage[1] #초당 힐량
+    add_heal = bandage[2] #시전 완료시 추가 힐량
+    atk_idx = 0 # 현재 몇번째 공격인지
     
-    for time in range(1, last_time + 1):     
+    for time in range(1, last_time + 1):
+        # 공격 받는 시간
         if time == attacks[atk_idx][0]:
-            hp -= attacks[atk_idx][1]
-            continuous_success = 0
+            hp -= attacks[atk_idx][1] # 공격받아서 체력이 깎임
+            continuous_success = 0 # 연속 붕대감기 초기화
             atk_idx += 1
             
-            if hp <= 0:
+            if hp <= 0: # 죽음
                 return -1
-                
-        else:
-            continuous_success += 1
-            hp = min(health, hp + heal)
-
-            if casting_time == continuous_success:
+        else: # 공격 안 받고 힐하는 시간
+            continuous_success += 1 # 연속 힐
+            hp = min(health, hp + heal) # 최대체력을 넘을 수 없음
+            
+            if casting_time == continuous_success: # 붕대감기 시전 끝
                 hp = min(health, hp + add_heal)
                 continuous_success = 0
-        
-    
+                
     return hp
